aca822ae04e28f7c561b52914a2e568d2d8a64bc
Gerrit Grunwald <han.solo@mac.com>
2016-01-11 12:27:47 +0100

    Added two color pickers which copy the clicked color as web and rgb color to the clipboard

---
+++ src/main/java/eu/hansolo/colors/FlatUiColorPicker.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2016 by Gerrit Grunwald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.hansolo.colors;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Scene;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * User: hansolo
+ * Date: 11.01.16
+ * Time: 12:22
+ */
+public class FlatUiColorPicker extends Application {
+    private static final int BOX_WIDTH  = 90;
+    private static final int BOX_HEIGHT = 90;
+    private GridPane grid;
+
+    @Override public void init() {
+        grid = new GridPane();
+
+        Font font = Font.font(9);
+        int  col  = 0;
+        int  row  = 0;
+        for (FlatUi color : FlatUi.values()) {
+            String name     = color.name().replace("_", " ");
+            String hex      = color.COLOR.toString().replace("0x", "");
+            String hexRed   = hex.substring(0, 2).toUpperCase();
+            String hexGreen = hex.substring(2, 4).toUpperCase();
+            String hexBlue  = hex.substring(4, 6).toUpperCase();
+            String intRed   = Integer.toString(Integer.parseInt(hexRed, 16));
+            String intGreen = Integer.toString(Integer.parseInt(hexGreen, 16));
+            String intBlue  = Integer.toString(Integer.parseInt(hexBlue, 16));
+            String text     = new StringBuilder().append(name)
+                                                 .append("\n")
+                                                 .append("#").append(hexRed).append(hexGreen).append(hexBlue).append("\n")
+                                                 .append("rgb(").append(intRed).append(",").append(intGreen).append(",").append(intBlue).append(")")
+                                                 .toString();
+
+            Label label = new Label(name);
+            label.setFont(font);
+            label.setAlignment(Pos.CENTER_LEFT);
+            label.setTextFill(Color.WHITE);
+            label.setMouseTransparent(true);
+
+            StackPane box = new StackPane(label);
+            box.setPrefSize(BOX_WIDTH, BOX_HEIGHT);
+            box.setAlignment(Pos.BOTTOM_LEFT);
+            box.setBackground(new Background(new BackgroundFill(color.COLOR, CornerRadii.EMPTY, Insets.EMPTY)));
+            box.setOnMousePressed(event -> {
+                box.setScaleX(0.9);
+                box.setScaleY(0.9);
+                String clipboardContent = new StringBuilder().append("Color.web(\"#")
+                                                             .append(hexRed)
+                                                             .append(hexGreen)
+                                                             .append(hexBlue)
+                                                             .append("\");\n")
+                                                             .append("Color.rgb(")
+                                                             .append(intRed)
+                                                             .append(", ")
+                                                             .append(intGreen)
+                                                             .append(", ")
+                                                             .append(intBlue)
+                                                             .append(");").toString();
+
+                Clipboard        clipboard = Clipboard.getSystemClipboard();
+                ClipboardContent content   = new ClipboardContent();
+                content.putString(clipboardContent);
+                clipboard.setContent(content);
+            });
+            box.setOnMouseReleased(event -> {
+                box.setScaleX(1.0);
+                box.setScaleY(1.0);
+            });
+
+            Tooltip tooltip = new Tooltip(text);
+            Tooltip.install(box, tooltip);
+            grid.add(box, col, row);
+            col++;
+            if (col > 4) {
+                col = 0;
+                row++;
+            }
+        }
+    }
+
+    @Override public void start(Stage stage) {
+        StackPane pane = new StackPane();
+        pane.getChildren().addAll(grid);
+
+        Scene scene = new Scene(pane);
+
+        stage.setTitle("Flat UI Colors");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override public void stop() {
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/eu/hansolo/colors/MaterialDesignColorPicker.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2016 by Gerrit Grunwald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.hansolo.colors;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Scene;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * User: hansolo
+ * Date: 11.01.16
+ * Time: 11:22
+ */
+public class MaterialDesignColorPicker extends Application {
+    private static final Pattern PATTERN    = Pattern.compile("(_[A]?[0-9]{2,3})");
+    private static final Matcher MATCHER    = PATTERN.matcher("");
+    private static final int     BOX_WIDTH  = 70;
+    private static final int     BOX_HEIGHT = 50;
+    private GridPane grid;
+
+    @Override public void init() {
+        grid = new GridPane();
+
+        Font font = Font.font(9);
+        int  col  = 0;
+        int  row  = 0;
+        for (MaterialDesign color : MaterialDesign.values()) {
+            String name     = color.name().replace("_", " ");
+            String hex      = color.COLOR.toString().replace("0x", "");
+            String hexRed   = hex.substring(0, 2).toUpperCase();
+            String hexGreen = hex.substring(2, 4).toUpperCase();
+            String hexBlue  = hex.substring(4, 6).toUpperCase();
+            String intRed   = Integer.toString(Integer.parseInt(hexRed, 16));
+            String intGreen = Integer.toString(Integer.parseInt(hexGreen, 16));
+            String intBlue  = Integer.toString(Integer.parseInt(hexBlue, 16));
+            String text     = new StringBuilder().append(name)
+                                                 .append("\n")
+                                                 .append("#").append(hexRed).append(hexGreen).append(hexBlue).append("\n")
+                                                 .append("rgb(").append(intRed).append(",").append(intGreen).append(",").append(intBlue).append(")")
+                                                 .toString();
+            MATCHER.reset(color.name());
+            String brightness = "";
+            while (MATCHER.find()) {
+                brightness = MATCHER.group(1).replace("_", " ");
+            }
+
+            Label label = new Label(color.name().replace("_", " ").replace(brightness, "") + "\n" + brightness);
+            label.setFont(font);
+            label.setAlignment(Pos.CENTER_LEFT);
+            label.setTextFill(Color.WHITE);
+            label.setMouseTransparent(true);
+
+            StackPane box = new StackPane(label);
+            box.setPrefSize(BOX_WIDTH, BOX_HEIGHT);
+            box.setAlignment(Pos.BOTTOM_LEFT);
+            box.setBackground(new Background(new BackgroundFill(color.COLOR, CornerRadii.EMPTY, Insets.EMPTY)));
+            box.setOnMousePressed(event -> {
+                box.setScaleX(0.9);
+                box.setScaleY(0.9);
+                String clipboardContent = new StringBuilder().append("Color.web(\"#")
+                                                             .append(hexRed)
+                                                             .append(hexGreen)
+                                                             .append(hexBlue)
+                                                             .append("\");\n")
+                                                             .append("Color.rgb(")
+                                                             .append(intRed)
+                                                             .append(", ")
+                                                             .append(intGreen)
+                                                             .append(", ")
+                                                             .append(intBlue)
+                                                             .append(");").toString();
+
+                Clipboard        clipboard  = Clipboard.getSystemClipboard();
+                ClipboardContent content    = new ClipboardContent();
+                content.putString(clipboardContent);
+                clipboard.setContent(content);
+            });
+            box.setOnMouseReleased(event -> {
+                box.setScaleX(1.0);
+                box.setScaleY(1.0);
+            });
+
+            Tooltip tooltip = new Tooltip(text);
+            Tooltip.install(box, tooltip);
+            grid.add(box, col, row);
+            row++;
+            if (col > 15) {
+                if (row == 11) {
+                    row = 0;
+                    col++;
+                }
+            } else {
+                if (row == 15) {
+                    row = 0;
+                    col++;
+                }
+            }
+        }
+    }
+
+    @Override public void start(Stage stage) {
+        StackPane pane = new StackPane();
+        pane.getChildren().addAll(grid);
+
+        Scene scene = new Scene(pane);
+
+        stage.setTitle("MaterialDesign Colors");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override public void stop() {
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
